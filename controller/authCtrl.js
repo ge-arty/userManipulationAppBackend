08c8a32b165ff37a7613fcbc782ef97d19e0a1d3
ge-arty <george.arutinyan@gmail.com>
2023-06-07 21:38:01 +0400

    fix3

--- controller/authCtrl.js
+++ controller/authCtrl.js
@@ -89,8 +89,14 @@ const updateUserCtrl = expressAsyncHandler(async (req, res) => {
 });
 
 const blockOrUnblockUserCtrl = expressAsyncHandler(async (req, res) => {
+  const idString = req.params.id;
+  console.log("id from params", _id);
   console.log("blockOrUnblockUserCtrl", req);
-  const { _id } = req.params.id;
+
+  const _id = new ObjectId(idString);
+
+  console.log("_id", _id);
+
   validateMongodbId(_id);
   const { isBlocked } = req.body;
   const blockedOrUnblockedUser = await User.findByIdAndUpdate(
